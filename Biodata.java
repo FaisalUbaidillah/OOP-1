223433c381085301815ca87e5ce6cf7d37c93260
FaisalUbaidillah <ubaidillah292@gmail.com>
2016-03-28 13:21:39 -1200

    OOP

---
+++ Biodata.java
@@ -0,0 +1,10 @@
+public class Biodata
+{
+
+public static void main (String[] args)
+
+{
+System.out.println("Nama : Faisal Ubaidillah TTL : Ciamis, 01 01 1995 Agama : Islam");
+
+}
+}
---
+++ Latihan.java
@@ -0,0 +1,24 @@
+public class Latihan{
+
+public static void main(String[] args)
+{
+
+Latihan nama = new Latihan();
+nama.namaSaya();
+nama.pertambahan();
+
+System.out.println(nama.namaSaya());
+
+}
+
+public String namaSaya(){
+return "Faisal Ubaidillah";
+
+}
+public void pertambahan(){
+int a = 1;
+int b = 2;
+System.out.println(a+b);
+
+}
+   }
