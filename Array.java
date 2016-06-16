71afe5d2ee7b2fe02b3295cbfccccf2845300650
Faisal Ubaidillah <ubaidillah292@gmail.com>
2016-06-15 15:15:50 -1200

    Add files via upload

--- Array.java
+++ Array.java
@@ -8,32 +8,23 @@ package array;
 
 /**
  *
- * @author ASUS
+ * @author Faisal
  */
 public class Array {
 
-   
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-       
-        int[] x;
+       int bil_1, bil_2, hasil;
+bil_1 = 10;
+bil_2 = 500;
+hasil = bil_1 + bil_2;
+System.out.print("Hasil Penjumlahan kedua bilangan :");
+System.out.println(hasil);
 
-        x = new int[4];
-        x[0] = 10;
-
-        x[1] = 11;
-
-        x[2] = 12;
-
-        x[3] = 13;
-
-        System.out.println("Index 0: "+ x[0]);
-
-        System.out.println("Index 1: "+ x[1]);
-
-        System.out.println("Index 2: "+ x[2]);
-
-        System.out.println("Iindex 3: "+ x[3]); 
-
-    }
+   }
     
-    }
+}
+ 
+    
